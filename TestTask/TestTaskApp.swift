ed4a7b73a310ad6a2e53196590a85510feff9f71
BorisKly <bklykavka@gmail.com>
2024-09-24 08:55:32 +0300

    Initial Commit

---
+++ TestTask/TestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskApp.swift
+//  TestTask
+//
+//  Created by Borys Klykavka on 24.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
